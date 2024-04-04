63d5d68541d654819777aa57f3cabb4848b8d9b1
Mani Mirjavadi <mani.mirjavadi@gmail.com>
2024-04-04 16:11:47 +0330

    Removed eslint_d

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,9 +5,7 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.diagnostics.rubocop,
+				null_ls.builtins.formatting.prettier, 
         null_ls.builtins.formatting.rubocop,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
