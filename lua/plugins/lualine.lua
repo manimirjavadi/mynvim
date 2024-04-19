10e0d949b5a5776ea7536ec156c7758acf0cc860
Mani Mirjavadi <mani.mirjavadi@gmail.com>
2024-04-19 13:51:15 +0330

    Updated vim-options for better lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,19 +1,19 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        icons_enabled = true,
-        theme = "nightfly",
-        sections = {
-          lualine_a = {
-            {
-              "filename",
-              path = 1,
-            },
-          },
-        },
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "dracula",
+				sections = {
+					lualine_a = {
+						{
+							"filename",
+							path = 1,
+						},
+					},
+				},
+			},
+		})
+	end,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,37 +1,19 @@
-vim.g.mapleader = " "
-vim.wo.number = true
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set laststatus=3")
+vim.cmd("set noshowcmd")
+vim.g.mapleader = " "
 vim.g.background = "light"
+
 vim.opt.swapfile = false
 
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
+-- Navigate vim panes better
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.opt.number = true
-
-vim.opt.title = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.showcmd = true
-vim.opt.cmdheight = 1
-vim.opt.laststatus = 2
-vim.opt.shell = "fish"
-vim.opt.inccommand = "split"
-vim.opt.ignorecase = true
-vim.opt.smarttab = true
-vim.opt.breakindent = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.wrap = false
-vim.opt.backspace = { "start", "eol", "indent" }
-vim.opt.path:append({ "**" })
-vim.opt.wildignore:append("*/node_modules/*")
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.splitkeep = "cursor"
-vim.opt.mouse = ""
-vim.api.nvim_set_option("clipboard", "unnamed")
-vim.o.cmdheight = 0
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+vim.wo.number = true
