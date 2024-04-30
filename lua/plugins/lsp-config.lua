cba6c51991610ad3a26c00a975b883a43fa7182b
Mani Mirjavadi <mani.mirjavadi@gmail.com>
2024-04-30 15:50:03 +0330

    Updated

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,48 +1,50 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "cssls", "gopls", "rust_analyzer", "pyright" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "cssls", "gopls", "rust_analyzer", "pyright" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-        filetypes = { "css", "scss" }
-			})
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+        filetypes = { "css", "scss" },
+      })
       lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
+        capabilities = capabilities,
+      })
       lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
-			})
+        capabilities = capabilities,
+      })
       lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
+        capabilities = capabilities,
+      })
 
-
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set({ "n", "v" }, "J", vim.diagnostic.open_float, {})
+      vim.keymap.set({ "n", "v" }, "l", vim.diagnostic.goto_next)
+      vim.keymap.set({ "n", "v" }, "L", vim.diagnostic.goto_prev)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
